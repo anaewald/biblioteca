dd86b345ad6cc3ce7805426de314fc47b684489d
caiogrijo <0064700@sesi-es.org.br>
2026-08-12 12:19:11 -0300

    salvar

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ import csv
 ARQUIVO = "livros.csv"
 
 #isso é para carregar os livros salvos
-def carregar_livros(titulo, autor, ano, codigo, status = "disponivel"):
-    livros = {
+def cadastrar_livros(titulo, autor, ano, codigo, status = "disponivel"):
+    livro = {
         "titulo": titulo,
         "autor": autor,
         "ano": ano,
-        "codigo": codigo
-        "status": = "disponivel"
-    }
-    livros.append(livro)
-    
+        "codigo": codigo,
+        "status":status
+        }
+    cabecacho = ["titulo", "autor", "ano", "codigo", "status"]
+    with open('livros.csv','a',encoding='UTF-8',newline='') as biblioteca:
+        escritor = csv.DictWriter(biblioteca,fieldnames=cabecacho)
+        escritor.writerow(livro)
+
+titulo = input("Digite o titulo: ")
+autor = input("Digite o autor: ")
+ano = input("Digite o ano: ")
+codigo = input("Digite o codigo: ")
+
+cadastrar_livros(titulo,autor,ano,codigo)
