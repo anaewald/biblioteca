e16cbfddc53a49d95f6a3dad4dffaa746a3cd0ee
caiogrijo <0064700@sesi-es.org.br>
2026-08-12 11:51:13 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import csv
+ARQUIVO = "livros.csv"
+
+#isso é para carregar os livros salvos
+def carregar_livros(titulo, autor, ano, codigo, status = "disponivel"):
+    livros = {
+        "titulo": titulo,
+        "autor": autor,
+        "ano": ano,
+        "codigo": codigo
+        "status": = "disponivel"
+    }
+    livros.append(livro)
+    
